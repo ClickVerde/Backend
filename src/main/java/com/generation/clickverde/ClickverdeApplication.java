3684c6da3a4016e30a48538cdec2ecc2779eb1ef
Henrique M. N. <henriquemancininevado@gmail.com>
2023-12-13 13:24:15 -0300

    Criar o Projeto Spring

---
+++ src/main/java/com/generation/clickverde/ClickverdeApplication.java
@@ -0,0 +1,13 @@
+package com.generation.clickverde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickverdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickverdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/clickverde/ClickverdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.clickverde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickverdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
